01aedbba19d0fda2f1cafd22faa6f0e4dcf9a725
chad-2024 <chad-2024@hotmail.com>
2024-05-10 16:46:32 +0200

    Improvements to W03: Project Milestone - The Chamber Discover Page

---
+++ chamber/scripts/navHamburger.js
@@ -0,0 +1,10 @@
+
+// NAV HAMBURGER
+const hamburgerButton = document.querySelector("#menu");
+const navbar = document.querySelector(".navbar");
+
+hamburgerButton.addEventListener("click", function () {
+    navbar.classList.toggle("displayMenu");
+    hamburgerButton.classList.toggle("display");
+});
+
