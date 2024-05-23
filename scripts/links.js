1a32b09ba6dfc127376a0849a3decdec9acab9f0
chad-2024 <chad-2024@hotmail.com>
2024-05-23 11:30:51 +0200

    Completed W05: Team Activity - JSON and the Weather API

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,29 @@
 const baseURL = "https://jdheslop.github.io/wdd230/";
 const linksURL = "https://jdheslop.github.io/wdd230/data/links.json";
+const weeksUL = document.querySelector("#weeks");
 
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
+    //console.log(data);
+
+    displayLinks(data.weeks);
 }
 
-gitLinks();
+function displayLinks(weeks) {
+    weeks.forEach((activities) => {
+        let week = document.createElement("li");
+        week.textContent = `${activities.week}: `
+    
+        activities.links.forEach((topic) => {
+            let item = document.createElement("a");
+            item.setAttribute("href", topic.url);
+            item.textContent = topic.title;
+            week.appendChild(item);
+        });
+        
+        weeksUL.appendChild(week);    
+    });
+}
+
+getLinks();
